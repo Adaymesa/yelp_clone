c7ccc71e52cd90f648c2bef5c5cfc009652413e4
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-23 19:31:07 +0100

    yelp v1 section 8 creating

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,18 @@
 class RestaurantsController < ApplicationController
   def index
-    # raise 'Hello from the index action'
+    @restaurants = Restaurant.all
+  end
+
+  def new
+     @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
