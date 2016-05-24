ec92d7186baf9417c732bc33b3999d12ca39e3a2
Ben Stein <bumstain@gmail.com>
2016-05-25 01:18:41 +1000

    Added validations for name length and ratings

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
