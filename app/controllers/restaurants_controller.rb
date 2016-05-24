480d03317e4f14f54f32f96a062f3c8bee2bfc42
Ben Stein <bumstain@gmail.com>
2016-05-24 21:36:43 +1000

    Can edit existing restaurant listings

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,10 +13,21 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit 
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update 
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
   end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -43,4 +43,19 @@ feature 'restaurants' do
     end
   end
 
+  context 'editing restaurants' do 
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'let a user edit a restaurant' do 
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
 end
